4bb1bb7b535329b0203b34e72828c73ab234c526
Gichere <francisgichere@gmail.com>
2021-03-16 22:21:00 +0300

    Added the EDA report, and RMArkdown report and several charts

--- g2m scripts.R
+++ g2m scripts.R
@@ -2,6 +2,13 @@
 library(tidyverse)
 library(lubridate)
 library(tidyr)
+library(dlookr)
+library(stringr)
+library(ggpubr)
+library(knitr)
+library(pastecs)
+library(reshape2)
+library(ggcorrplot)
 
 #loading the datasets
 cab<-read_csv("Cab_Data.csv")
@@ -53,9 +60,68 @@ glimpse(master_data)
 ## Dropping the rows with missing values
 master_clean<-master_data %>% drop_na()
 glimpse(master_clean)
+
+#Adding a calculated column for profit margin
+master_clean$Profit_margin<-(master_clean$`Price Charged` - master_clean$`Cost of Trip`)
+glimpse(master_clean)
 #Writing the final data set to be used in the analysis
 
 write_csv(master_clean, "g2m.csv")
 
 ###### Exploratory Data Analysis
 final_data<-read_csv("g2m.csv")
+head(final_data)
+
+summary(final_data)
+glimpse(final_data)
+
+###Defining Gender and Company as factors
+final_data$Company<-as.factor(final_data$Company)
+final_data$Gender<-as.factor(final_data$Gender)
+final_data$Payment_Mode<-as.factor(final_data$Payment_Mode)
+### Creating age groups
+final_data$Age_Group<-cut(final_data$Age, c(17,25,32,39,46,53,60,65),
+                          labels = c("18-25",
+                                     "26-32",
+                                     "33-39",
+                                     "40-46",
+                                     "47-53", 
+                                     "54-60",
+                                     "61+"))
+
+## Checking if the grouping has been done correctly
+table_1<-table(final_data$Age, final_data$Age_Group)
+view(table_1)
+
+################# Plots ######################
+## Distributions
+
+# Kilometer traveled 
+ggplot(final_data, mapping = aes(x=`KM Travelled`))+
+  geom_histogram(mapping = aes(color=Company), bins = 30)
+## Plotting Price charged per company
+ggplot(data = final_data, mapping = aes(y=`Price Charged`, x=Company))+
+  geom_boxplot(mapping = aes(color=Company))+
+  theme_pubclean()+
+  ggtitle("Plot of Price Charged per Company")+
+  theme(legend.position = "none")
+
+## Plot of cost of trip per company
+ggplot(data = final_data, mapping = aes(y=`Cost of Trip`, x=Company))+
+  geom_boxplot(mapping = aes(color=Company))+
+  theme_pubclean()+
+  ggtitle("Plot of Cost of Trip per Company")+
+  theme(legend.position = "none")
+
+## Plot of Profit margin per company
+ggplot(data = final_data, mapping = aes(y=Profit_margin, x=Company))+
+  geom_boxplot(mapping = aes(color=Company))+
+  theme_pubclean()+
+  ggtitle("Plot of Profit per Company")+
+  theme(legend.position = "none")
+
+ggplot(data = final_data, mapping = aes(y=Profit_margin, x=Gender))+
+  geom_boxplot()+
+  theme_pubclean()+
+  ggtitle("Plot of Profit per Company")+
+  theme(legend.position = "none")
